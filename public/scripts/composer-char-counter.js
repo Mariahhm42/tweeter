2083a7e2a0f866c1fb69505f94069fa40af7c994
Mariam Agboola <mariahhm42@gmail.com>
2024-12-16 21:44:10 -0700

    implemented the character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-    // --- our code goes here ---
+  // Handle the input event for the textarea
+  $('#tweet-text').on('input', function() {
+    // Get the current length of the tweet text
+    var charCount = $(this).val().length;
+    
+    // Get the counter element to update
+    var counter = $(this).siblings('div').find('.counter');
+    // Calculate the remaining characters
+    var remaining = 140 - charCount;
+    // Update the counter
+    counter.text(remaining);
+    // Change the counter color if it exceeds the limit
+    if (remaining < 0) {
+      counter.addClass('over-limit');
+    } else {
+      counter.removeClass('over-limit');
+    }
   });
+});
