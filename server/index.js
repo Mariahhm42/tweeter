2c86770ee02d3108545a6a96b8004b15b839697d
Mariam Agboola <mariahhm42@gmail.com>
2025-01-13 03:38:43 -0700

    fixed the console.log message in index.js file

--- server/index.js
+++ server/index.js
@@ -1,19 +1,17 @@
 "use strict";
 
 // Basic express setup:
-const PORT = 8080;
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
 
+const PORT          = 8080;
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const app           = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public")); // Serves static files (e.g., HTML, CSS, JS) from the 'public' directory
-app.set('view engine', 'ejs');  // use EJS as the templating engine
-
+app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db.js");
+const db = require("./lib/in-memory-db");
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -25,22 +23,15 @@ const db = require("./lib/in-memory-db.js");
 const DataHelpers = require("./lib/data-helpers.js")(db);
 
 // Update the dates for the initial tweets (data-files/initial-tweets.json).
-require("./lib/date-adjust.js")();
+require("./lib/date-adjust")();
 
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets.js")(DataHelpers);
+const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
-// Default route to serve the main Tweeter page
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-
-// Start the server
 app.listen(PORT, () => {
-  console.log("Tweeter app listening on port " + PORT);
+  console.log("Holla! Tweeter app is listening on port " + PORT);
 });
